02780ce43f6a9feb926927d08db8ff904363a466
Felipe Cardoso <felipedclc@gmail.com>
2021-11-03 20:20:56 -0300

    api express e middleware de erro

---
+++ src/api/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const middlewareError = require('../middlewares/error');
+const UserController = require('../controllers/UserController');
+
+const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(cors());
+
+app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' })); // teste
+
+app.post('/researches', UserController.createSearchResults);
+app.get('/researches', UserController.findAllResearches);
+
+app.use(middlewareError);
+
+module.exports = app;
