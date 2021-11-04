9decc82818b2df5c2493e44511c1b349e9a72685
Felipe Cardoso <felipedclc@gmail.com>
2021-11-04 09:09:11 -0300

    default route /

--- src/api/app.js
+++ src/api/app.js
@@ -9,10 +9,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 
-app.post('/', UserController.createSearchResults);
-app.get('/', UserController.findAllResearches);
+app.get('/', (_, res) => res.send('enter route /researches'));
 
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' })); // teste
+
+app.post('/researches', UserController.createSearchResults);
+app.get('/researches', UserController.findAllResearches);
 
 app.use(middlewareError);
 
