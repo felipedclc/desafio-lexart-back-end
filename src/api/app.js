9da16f2f7f9fb461d6e07fb9caa54ee23ac03848
Felipe Cardoso <felipedclc@gmail.com>
2021-11-03 22:39:23 -0300

    teste heroku

--- src/api/app.js
+++ src/api/app.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 
-app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' })); // teste
+app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
 
 app.post('/researches', UserController.createSearchResults);
 app.get('/researches', UserController.findAllResearches);
