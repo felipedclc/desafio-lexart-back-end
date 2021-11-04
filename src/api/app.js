37ff785f9e05de569f35ab8f08a11511daded5d8
Felipe Cardoso <felipedclc@gmail.com>
2021-11-03 22:55:52 -0300

    mudando rota

--- src/api/app.js
+++ src/api/app.js
@@ -9,10 +9,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 
-app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' })); // teste
+app.post('/', UserController.createSearchResults);
+app.get('/', UserController.findAllResearches);
 
-app.post('/researches', UserController.createSearchResults);
-app.get('/researches', UserController.findAllResearches);
+app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' })); // teste
 
 app.use(middlewareError);
 
